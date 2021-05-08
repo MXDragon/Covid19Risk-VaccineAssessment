5bf4c6f474e9a7413d197a9ccb1c8a050629a46b
Diana Daghlas <dianadaghlas@yahoo.com>
2021-05-08 13:35:30 -0400

    Partial function for global covid case counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 var timeDisplayEl = $('#time-display');
-var currentCov19CasesEl = $('#"currentCov19Cases"');
-// var currentVaccineDistributedEl = $('#currentVaccineDistributed');
+// var getCovidData = $('#getCovidData');
+// var getVaccineData = $('#getVaccineData');
 var searchFormEl = document.querySelector('#search-form');
-var submitFormEl = document.querySelector('#submit-form');
-var searchButton = document.querySelector('#search-button');
+var covidGlobalEl = document.querySelector('#covid-global');
 
+var submitFormEl = document.querySelector('#submit-form');
 // handle displaying the time
 function displayTime() {
   // The way we want the date to look
@@ -14,4 +14,29 @@ function displayTime() {
   timeDisplayEl.text(rightNow);
 }
 
-  setInterval(displayTime, 1000);
+setInterval(displayTime, 1000);
+
+function getCovidData() {
+  var results = fetch("https://api.covid19api.com/summary")
+  // console.log(results)
+  .then(response => response.json())
+  .then(summary => showSummary(summary.results));
+}
+
+getCovidData();
+
+function init() {
+
+var ip=  fetch(" https://www.cloudflare.com/cdn-cgi/trace").then(res => res.text()).then(data => console.log(data))
+
+
+fetch("http://api.ipapi.com/" + ip + "?access_key=eee64595bb8ff68b058ee670d467d2e3")
+
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+}
+init()
