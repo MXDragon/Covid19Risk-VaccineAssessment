fb8320de9c7e4ce4dd48a8db2b21b8744dc471af
hurondragon <bentzbry@gmail.com>
2021-05-11 14:13:07 -0400

    Added map zoom, hazmat icon, variables for data

---
+++ assets/js/maps.js
@@ -0,0 +1,106 @@
+var mapBox = document.querySelector('#map');
+var userLatitude = "";
+var userLongitude = "";
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYmVudHpicnkiLCJhIjoiY2tvZ2o5cm5iMGZrcTJvbngwZnpzYW9yMyJ9.lFzRDHzMnkWAhcUuY-6POw';
+    var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/dark-v10',
+    center: end,
+    });
+    var geojson = {
+        'type': 'FeatureCollection',
+        'features': [
+        {
+        'type': 'Feature',
+        'properties': {
+        'message': 'Foo',
+        'iconSize': [60, 60]
+        },
+        'geometry': {
+        'type': 'Point',
+        'coordinates': [userLatitude, userLongitude]
+        }}
+        ]
+    };
+        
+var start = [0,0];
+var end = ""  
+
+function init(){
+    function json(url) {
+    return fetch(url).then(res => res.json());
+    }
+        
+    let apiKey = '9692d1c3f5905c16f6c3847aabe964849cafe1836e79a8d817edcd25';
+    json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
+        console.log(data)
+    
+        //getting Lat and Long for Map
+        userLatitude = data.latitude;
+        userLongitude = data.longitude;
+        end = [userLatitude, userLongitude];
+        
+        console.log(userLongitude);
+        console.log(userLatitude);
+    })
+    
+    var isAtStart = true;
+ 
+    // depending on whether we're currently at point a or b, aim for
+    // point a or b
+    var target = isAtStart ? end : start;
+ 
+    // and now we're at the opposite point
+    isAtStart = !isAtStart;
+    console.log("Lat and Lon Target is: " + target);
+
+    map.flyTo({
+        // These options control the ending camera position: centered at
+        // the target, at zoom level 9, and north up.
+        
+        center: target,
+        zoom: 9,
+        bearing: 0,
+         
+        // These options control the flight curve, making it move
+        // slowly and zoom out almost completely before starting
+        // to pan.
+        speed: 0.2, // make the flying slow
+        curve: 1, // change the speed at which it zooms out
+         
+        // This can be any easing function: it takes a number between
+        // 0 and 1 and returns another number between 0 and 1.
+        easing: function (t) {
+        return t;
+        },
+         
+        // this animation is considered essential with respect to prefers-reduced-motion
+        essential: true
+        });
+    
+    };
+
+    // Add markers to the map.
+    geojson.features.forEach(function (marker) {
+    // Create a DOM element for each marker.
+    var el = document.createElement('div');
+    el.className = 'marker';
+    el.style.backgroundImage =
+    'url(https://img.icons8.com/emoji/96/000000/biohazard-emoji.png)';
+    el.style.width = marker.properties.iconSize[0] + 'px';
+    el.style.height = marker.properties.iconSize[1] + 'px';
+    el.style.backgroundSize = '100%';
+     
+    el.addEventListener('click', function () {
+    window.alert(marker.properties.message);
+    });
+     
+    // Add markers to the map.
+    new mapboxgl.Marker(el)
+    .setLngLat(marker.geometry.coordinates)
+    .addTo(map);
+    });
+
+
+init();
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,56 +8,70 @@ var submitFormEl = document.querySelector('#submit-form');
 var country4Vax = "";
 var globalIP ="";
 var globalConfirmedVax =""
+var countryCode=""
+var continent_name=""
+var postalCode = ""
+var region = ""
+var regionCode = ""
+var newConfirmed =""
+var newDeaths = ""
+var totalDeaths = ""
+var newRecovered = ""
+var regionActive = ""
+var regionConfirmed = ""
+var regionDeaths =""
 
+
+//Was working, now not working for some reason, believe the site changed
 function getVaccineData(){
- fetch( 'https://covid-api.mmediagroup.fr/v1/vaccines?country=' + country4Vax )
- .then(function (response) {
+  fetch( 'https://covid-api.mmediagroup.fr/v1/vaccines?country=' + country4Vax )
+  .then(function (response) {
   return response.json();
-})
-.then(function (data) {
+  })
+  .then(function (data) {
   // console.log('Fetch Response \n-------------');
   console.log(data);
-  
   //Are doing stuff with the data here. So. Getting Global Vax data.
   globalConfirmedVax = data.All.administered;
   console.log(globalConfirmedVax);
-
-  
-})
-.catch(function (err) {
+  })
+  .catch(function (err) {
   console.log('error: ' + err);
-});
+  });
 }
 
 function getCovidGlobal(){
   fetch(requestUrl)
   .then(function (response) {
-   return response.json();
- })
- .then(function (data) {
-   // console.log('Fetch Response \n-------------');
-   console.log(data);
-   
-   //Are doing stuff with the data here. So. Getting Global Vax data.
-   globalConfirmedCovid = data.Global.TotalConfirmed;
-   console.log(globalConfirmedCovid);
-  
-   // calling the displayCovidGlobal function
-   displayCovidGlobal(globalConfirmedCovid);
+    return response.json();
   })
- .catch(function (err) {
-   console.log('error: ' + err);
- });
- }
+  .then(function (data) {
+    // console.log('Fetch Response \n-------------');
+    console.log(data);
+    //Are doing stuff with the data here. So. Getting Global Vax data.
+    newConfirmed = data.Global.NewConfirmed;
+    newDeaths = data.Global.NewDeaths;
+    totalDeaths = data.Global.TotalDeaths;
+    newRecovered = data.Global.NewRecovered;
+    globalConfirmedCovid = data.Global.TotalConfirmed;
+    console.log(globalConfirmedCovid);
+    // calling the displayCovidGlobal function
+    displayCovidGlobal(globalConfirmedCovid);
+  })
+  .catch(function (err) {
+    console.log('error: ' + err);
+  });
+}
 
  // displaying covid data
- function displayCovidGlobal(confirmedCases) {
-  var covidGlobalConfirmed1 = document.querySelector('#get-covid-data');
-  covidGlobalConfirmed1.append(confirmedCases);
- }
+function displayCovidGlobal(confirmedCases) {
+var covidGlobalConfirmed1 = document.querySelector('#get-covid-data');
+covidGlobalConfirmed1.append(confirmedCases);
+}
 
- getCovidGlobal();
+getCovidGlobal();
 
+ //would like to offload the GEOIP stuff called at the beginning of init here but having some syntax problems
 function getGeoIP(){
   
 }
@@ -77,12 +91,27 @@ function covidData(){
     .then(function (data) {
       // console.log('Fetch Response \n-------------');
       console.log(data);
+      
+      //must do some logic to get the right region
+      for ( i = data.length ; i < data.length ; i--){
+        if (data[i].Province === region){
+          regionActive = data[i].Active;
+          regionConfirmed = data[i].Confirmed;
+          regionDeaths = data[i].Deaths;
+          console.log("Region Deaths: " + regionDeaths)
+          console.log("Region Active: " + regionActive)
+          console.log("Region Confirmed : " + regionConfirmed)
+          return;
+        }
+      }
+
       console.log(data[0].Confirmed);
       //Are doing stuff with the data here. So.
       var TEMPcovidGlobalConfirmed=data[0].Confirmed;
       covidGlobalConfirmed.text(TEMPcovidGlobalConfirmed);
+
       return covidGlobalConfirmed.json();
-  
+      
       //appendData(data);
     })
     .catch(function (err) {
@@ -100,25 +129,36 @@ function displayTime() {
   timeDisplayEl.text(rightNow);
 }
 
-
+//helper class for displayTime
 setInterval(displayTime, 1000);
 
+//logic we call when we load the page. We can use logic calls elsewhere like buttons and dropdowns.
 function init() {
 
-  //var getIP=  fetch(" https://www.cloudflare.com/cdn-cgi/trace").then(res => res.text()).then(data => console.log(data))
+//var getIP=  fetch(" https://www.cloudflare.com/cdn-cgi/trace").then(res => res.text()).then(data => console.log(data))
 
-  function json(url) {
-  return fetch(url).then(res => res.json());
-  }
 
-  let apiKey = '9692d1c3f5905c16f6c3847aabe964849cafe1836e79a8d817edcd25';
-  json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
+
+function json(url) {
+return fetch(url).then(res => res.json());
+}
+
+let apiKey = '9692d1c3f5905c16f6c3847aabe964849cafe1836e79a8d817edcd25';
+json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
     console.log(data)
 
     globalIP = data.ip
-
+    //getting Lat and Long for Map
+    userLatitude = data.latitude
+    userLongitude = data.longitude
+    countryCode = data.country_code;
+    continent_name = data.continent_name;
+    postalCode = data.postal;
+    region = data.region;
+    regionCode = data.region_code;
     //will use country4Vax in the Vaccine call
     country4Vax = data.country_name;
+    console.log("country4vax: " + country4Vax);
 
     //Getting Covid Data for country
     //Converting country name so Covid will accept it, replace spaces with dashes and lower case
@@ -133,7 +173,7 @@ function init() {
     //Now we can call the Covid protocol using this information
     covidData();
     return;
-});
+  });
     //str = str.replace(/\s+/g, '-');
     // console.log("Global IP: " +globalIP);
     // console.log(data.ip);
@@ -144,6 +184,8 @@ function init() {
     getVaccineData();
     return;
 };
+
+
 
 init();
 
